aa6d1734a187f17bd80e148e31cf00651b67ce4d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers, no-undefined */
 import {test} from "tap"
 
-import isObject from "./source.js"
+import isNil from "./source"
 
 test(({notOk, end}) => {
   notOk(isObject(null))
